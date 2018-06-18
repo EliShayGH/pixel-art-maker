80b6ac49f3328df903f7fd0d1b1e937c86613a20
Eli Shay <El_mn123@hotmail.com>
2018-06-18 06:36:42 +0300

    bug fixed - EmptyMatrix() function was added

--- js/designs.js
+++ js/designs.js
@@ -3,33 +3,12 @@
 
 // When size is submitted by the user, call makeGrid()
 
-const sizePickerElement = document.querySelector('#sizePicker');
-const rows_input = document.querySelector('#inputHeight');
-const columns_input = document.querySelector('#inputWidth');
-const button = document.querySelector('#buttonSubmit');
-
-button.addEventListener('click',function(event){
-    console.log('click');
-    makeGrid();
-    event.preventDefault();
-    const rows = rows_input.value;
-    const columns = columns_input.value;
-    console.log('hello my matrix is ' + rows + '/' + columns);
-});
-//
-// sizePickerElement.addEventListener('submit',function(event){
-//     console.log('sumbited');
-//     event.preventDefault();
-// });
-
-
-function makeGrid() {
+document.querySelector('#buttonSubmit').addEventListener('click',function makeGrid(event) {
   const rows = document.querySelector('#inputHeight').value;
   const columns = document.querySelector('#inputWidth').value;
   const table = document.querySelector('#pixelCanvas');
 
-  // document.querySelector(table).remove('tr');
-  // document.querySelector(table).remove('td');
+  EmptyMatrix();
 
   for (let i = 0; i < rows; ++i)
   {
@@ -40,6 +19,18 @@ function makeGrid() {
       let td_element = document.createElement('td');
       tr_element.appendChild(td_element);
     }
+
     table.appendChild(tr_element);
+
+    event.preventDefault();
+  }
+});
+
+function EmptyMatrix() {
+  const tr_node_list = document.querySelectorAll('table tr');
+
+  for (let i = 0; i < tr_node_list.length; ++i)
+  {
+    document.querySelector('table').removeChild(tr_node_list[i]);
   }
 }
